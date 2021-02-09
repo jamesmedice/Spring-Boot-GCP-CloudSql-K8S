afd2e0378de7762e7b901c30b89f91c989a7fa93
TOSS <tiago.medice@gft.com>
2021-02-09 22:03:57 +0100

    rel_1

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+drop table if exists inventory;
+
+create table inventory
+(
+    id              integer     NOT NULL AUTO_INCREMENT,
+    itemName        varchar(50) NOT NULL,
+    itemPrice       float,
+    itemDescription varchar(50),
+    itemSupplier    varchar(50) NOT NULL DEFAULT 'Factory',
+    quantity        integer,
+    primary key (id)
+);
