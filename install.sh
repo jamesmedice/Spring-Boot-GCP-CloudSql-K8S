b5d070cde97077a2cc5cb67de2d37779016c5d51
TOSS <tiago.medice@gft.com>
2021-02-10 13:00:32 +0100

    rel_1

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kubectl  create  secret  generic  user-credentials  --from-literal=username=james  --from-literal=password=passjames
---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO inventory (itemName, itemPrice, itemDescription, itemSupplier, quantity) VALUES ("Milk", 40.00, "Milk", "National Milk Suppliers", 10);
+INSERT INTO inventory (itemName, itemPrice, itemDescription, itemSupplier, quantity) VALUES ("Bread", 30.00, "Bread", "Company Factory", 5);
