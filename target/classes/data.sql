a47bd3b4a50a8acca7aa9200c234d7915e3c75c9
TOSS <tiago.medice@gft.com>
2021-02-10 13:01:52 +0100

    rel_1

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO inventory (itemName, itemPrice, itemDescription, itemSupplier, quantity) VALUES ("Milk", 40.00, "Milk", "National Milk Suppliers", 10);
-INSERT INTO inventory (itemName, itemPrice, itemDescription, itemSupplier, quantity) VALUES ("Bread", 30.00, "Bread", "Company Factory", 5);
