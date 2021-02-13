a9198953ca6106441816a5ce3f3472b69e828b9e
TOSS <tiago.medice@gft.com>
2021-02-13 15:30:05 +0100

    rel

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl  create  secret  generic  user-credentials  --from-literal=username=james  --from-literal=password=passjames
+kubectl  create  secret  generic  user-credentials  --from-literal=username=james  --from-literal=password=james12345
