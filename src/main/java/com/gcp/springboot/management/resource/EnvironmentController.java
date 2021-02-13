0d9e257e932be01d5dd87795d8cf20129386ce2d
TOSS <tiago.medice@gft.com>
2021-02-13 12:51:48 +0100

    rel

--- src/main/java/com/gcp/springboot/management/resource/EnvironmentController.java
+++ src/main/java/com/gcp/springboot/management/resource/EnvironmentController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,9 +17,6 @@ public class EnvironmentController {
 	@Autowired
 	Environment environment;
 
-	@Value("${user_profile}")
-	private String profile;
-
 	@RequestMapping(value = "/envVars", method = RequestMethod.GET)
 	public Map<String, String> envVars() {
 		logger.info("microservice envVars() invoked");
@@ -31,12 +27,6 @@ public class EnvironmentController {
 	public String[] activeProfiles() {
 		logger.info("microservice activeProfiles() invoked");
 		return environment.getActiveProfiles();
-	}
-
-	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	public String profile() {
-		logger.info(profile);
-		return profile;
 	}
 
 }
